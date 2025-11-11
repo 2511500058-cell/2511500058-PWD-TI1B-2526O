f00d8ff80a20a71ef9bd45d618c46a5d74519ea0
Muhammad Tio Saputra <2511500058@mahasiswa.atmaluhur.ac.id>
2025-11-11 20:38:20 +0700

    isi get_proses.php

--- pertemuan-07/get_proses.php
+++ pertemuan-07/get_proses.php
@@ -1,3 +1,7 @@
 <?php
-echo $_GET["txtNama"];
+session_start();
+$_SESSION["nama"] = $_GET["txtNama"];
+$_SESSION["email"] = $_GET["txtEmail"];
+$_SESSION["pesan"] = $_GET["txtPesan"];
+header("Location: get.php");
 ?>
